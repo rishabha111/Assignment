ea1448972500e3d385ea8afe02b8325ad5cecff6
rishabha111 <yadavrishabha3499@gmail.com>
2024-05-01 23:29:06 +0530

    code

--- src/CartScreen.jsx
+++ src/CartScreen.jsx
@@ -1,10 +1,64 @@
 
 
+// import React, { useState, useEffect } from "react";
+// import { styled } from '@mui/material/styles';
+// import Box from '@mui/material/Box';
+// import Paper from '@mui/material/Paper';
+// import Grid from '@mui/material/Grid';
+
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+//   ...theme.typography.body2,
+//   padding: theme.spacing(1),
+//   textAlign: 'center',
+//   color: theme.palette.text.secondary,
+// }));
+// const CartScreen = () => {
+//     const [cartProducts, setCartProducts] = useState([]);
+
+//     useEffect(() => {
+//         const storedProducts = localStorage.getItem("addedProducts");
+//         if (storedProducts) {
+//             const parsedProducts = JSON.parse(storedProducts);
+//             setCartProducts(parsedProducts);
+//         }
+//     }, []);
+
+//     return (
+//      <div style={{backgroundColor:"#E9E6DF"}}>
+//   <h1> My Cart</h1>
+//   <div>
+//     {cartProducts.map((product) => (
+//       <div key={product.id} style={{ marginBottom: '20px' }}>
+//         <img
+//           src={product.image}
+//           alt={product.name}
+//           height="200"
+//           width="auto"
+//           style={{ maxWidth: '20%', height: 'auto', borderRadius: '10px' }}
+//         />
+//         <div>
+//           <p>{product.title}</p>
+//           <p>Product Type : {product.category.name}</p>
+//           <p>{product.description}</p>
+//           <p style={{ fontWeight: "500" }}>Price: ${product.price}</p>
+//         </div>
+//       </div>
+//     ))}
+//   </div>
+// </div>
+//   );
+// }
+// export default CartScreen;
+
+
+
 import React, { useState, useEffect } from "react";
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import axios from 'axios'; // Import Axios for making HTTP requests
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -13,8 +67,10 @@ const Item = styled(Paper)(({ theme }) => ({
   textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
+
 const CartScreen = () => {
     const [cartProducts, setCartProducts] = useState([]);
+const dummyImageURL = 'https://dummyimage.com/600x400/000/fff';
 
     useEffect(() => {
         const storedProducts = localStorage.getItem("addedProducts");
@@ -24,33 +80,75 @@ const CartScreen = () => {
         }
     }, []);
 
+
+//     const handleCheckout = async () => {
+//   try {
+//     const flattenedProducts = cartProducts.map((product) => ({
+//       ...product,
+//       image: dummyImageURL, // Use dummy image URL
+//     }));
+//     const response = await axios.post(
+//       'http://localhost:3000/api/create-checkout-session',
+//       flattenedProducts
+//     );
+//     console.log("@@@@@@@", response.data); // Assuming your server returns some data
+//     // Handle success or update UI accordingly
+//   } catch (error) {
+//     console.error('Error:', error);
+//     // Handle error or update UI accordingly
+//   }
+  // };
+  
+  const handleCheckout = async () => {
+  try {
+    const flattenedProducts = cartProducts.map((product) => ({
+      ...product,
+      image: dummyImageURL, // Use dummy image URL
+    }));
+    
+    const response = await fetch('http://localhost:7000/api/create-checkout-session', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(flattenedProducts)
+    });
+
+    const responseData = await response.json();
+    console.log("111111111", responseData); // Assuming your server returns some data
+    // Handle success or update UI accordingly
+  } catch (error) {
+    console.error('Error:', error);
+    // Handle error or update UI accordingly
+  }
+};
+
     return (
      <div style={{backgroundColor:"#E9E6DF"}}>
-  <h1> My Cart</h1>
-  <div>
-    {cartProducts.map((product) => (
-      <div key={product.id} style={{ marginBottom: '20px' }}>
-        <img
-          src={product.image}
-          alt={product.name}
-          height="200"
-          width="auto"
-          style={{ maxWidth: '20%', height: 'auto', borderRadius: '10px' }}
-        />
+        <h1>My Cart</h1>
+        <button onClick={handleCheckout}>Checkout</button>
         <div>
-          <p>{product.title}</p>
-          <p>Product Type : {product.category.name}</p>
-          <p>{product.description}</p>
-          <p style={{ fontWeight: "500" }}>Price: ${product.price}</p>
+            {cartProducts.map((product) => (
+            <div key={product.id} style={{ marginBottom: '20px' }}>
+                <img
+                src={product.image}
+                alt={product.title}
+                height="200"
+                width="auto"
+                style={{ maxWidth: '20%', height: 'auto', borderRadius: '10px' }}
+                />
+                <div>
+                <p>{product.title}</p>
+                <p>Product Type : {product.category.name}</p>
+                <p>{product.description}</p>
+                <p style={{ fontWeight: "500" }}>Price: ${product.price}</p>
+                </div>
+            </div>
+            ))}
         </div>
-      </div>
-    ))}
-  </div>
-</div>
-  );
-}
+     </div>
+    );
+};
+
 export default CartScreen;
-
-
-
 
--- src/CategoriesScreen.jsx
+++ src/CategoriesScreen.jsx
@@ -1,5 +1,145 @@
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// function CategoriesScreen() {
+//   const [products, setProducts] = useState([]);
+//   const [sortOrder, setSortOrder] = useState("");
+//   const [minPrice, setMinPrice] = useState(0);
+//   const [maxPrice, setMaxPrice] = useState(1000);
+//   const [categoryId, setCategoryId] = useState("");
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+//   const [currentPage, setCurrentPage] = useState(1);
+//   const productsPerPage = 18;
+//   const defaultImage = "https://i.imgur.com/cHddUCu.jpeg";
+
+//   useEffect(() => {
+//     fetchProducts();
+//   }, [categoryId, minPrice, maxPrice, currentPage]);
+
+//   const handleSort = (order) => {
+//     if (!Array.isArray(products) || products.length === 0) {
+//       return;
+//     }
+
+//     const sortedProducts = [...products];
+
+//     if (order === "minToMax") {
+//       sortedProducts.sort((a, b) => a.price - b.price);
+//     } else if (order === "maxToMin") {
+//       sortedProducts.sort((a, b) => b.price - a.price);
+//     }
+
+//     setProducts(sortedProducts);
+//     setSortOrder(order);
+//   };
+
+//   const fetchProducts = async () => {
+//     setLoading(true);
+//     setError(null);
+//     try {
+//       const offset = (currentPage - 1) * productsPerPage;
+//       const limit = productsPerPage;
+//       const response = await axios.get(
+//         `https://api.escuelajs.co/api/v1/products/?price_min=${minPrice}&price_max=${maxPrice}&categoryId=${categoryId}&offset=${offset}&limit=${20}`
+//       );
+//       setProducts(response.data);
+//     } catch (error) {
+//       setError("Error fetching products. Please try again later.");
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+
+//   const indexOfLastProduct = currentPage * productsPerPage;
+//   const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+//   const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+
+//   const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+//   return (
+//     <div style={{marginleft:'10px',marginRight:'10px',backgroundColor:"#E9E6DF"}}>
+//       <h1 style={{ textAlign: "center" }}>Categories</h1>
+//       <div style={{ display: "flex", justifyContent: "center", marginBottom: "20px" }}>
+//         <button style={{ marginRight: "10px" }} onClick={() => handleSort("maxToMin")}>Max to Min Price</button>
+//         <button onClick={() => handleSort("minToMax")}>Min to Max Price</button>
+//       </div>
+
+//       <div style={{ textAlign: "center", marginBottom: "20px" }}>
+//         <label>
+//           Min Price:
+//           <input type="number" value={minPrice} onChange={(e) => setMinPrice(e.target.value)} />
+//         </label>
+//         <label style={{ marginLeft: "10px" }}>
+//           Max Price:
+//           <input type="number" value={maxPrice} onChange={(e) => setMaxPrice(e.target.value)} />
+//         </label>
+//       </div>
+
+//       {loading ? (
+//         <div style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "200px" }}>
+//         </div>
+//       ) : error ? (
+//         <p style={{ textAlign: "center", color: "red" }}>{error}</p>
+//       ) : (
+//         <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))", gap: "20px" }}>
+//           {currentProducts.map((product) => (
+//             <div key={product.id} style={{ border: "1px solid #ccc", padding: "10px", textAlign: "center", borderRadius: '10px' }}>
+//               {product.images && product.images.length > 0 && !product.images[0].startsWith('["') && !product.images[0].endsWith('"]') ? (
+//                 <img
+//                   src={product.images[0]}
+//                   alt={`Product 1`}
+//                   height="150"
+//                   width="auto"
+//                   loading="lazy"
+//                   style={{ borderRadius: '10px' }}
+//                 />
+//               ) : (
+//                 <img
+//                   src={defaultImage}
+//                   alt="Image Not Found"
+//                   height="150"
+//                   width="auto"
+//                   loading="lazy"
+//                   style={{ borderRadius: '10px' }}
+//                 />
+//               )}
+//               <h3>{product.title}</h3>
+//               <p>${product.price}</p>
+//             </div>
+//           ))}
+//         </div>
+//       )}
+
+//       <ul style={{display: 'flex',
+//             listStyle: 'none',
+//         padding: '0px',
+//             justifyContent: 'center',
+//         marginTop: '20px',
+//         margin:'20px',
+//       }}>
+//             {Array.from({ length: Math.ceil(products.length / productsPerPage) }).map((_, index) => (
+//               <li key={index} className={currentPage === index + 1 ? 'active' : ''}>
+//                 <a onClick={() => paginate(index + 1)}>{index + 1}</a>
+//               </li>
+//             ))}
+//           </ul>
+//     </div>
+//   );
+// }
+
+// export default CategoriesScreen;
+
+
+
+
+
+
+
+
+
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+
 function CategoriesScreen() {
   const [products, setProducts] = useState([]);
   const [sortOrder, setSortOrder] = useState("");
@@ -9,6 +149,7 @@ function CategoriesScreen() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
+  const [selectedCategory, setSelectedCategory] = useState(""); // Added state for selected category
   const productsPerPage = 18;
   const defaultImage = "https://i.imgur.com/cHddUCu.jpeg";
 
@@ -40,8 +181,9 @@ function CategoriesScreen() {
       const offset = (currentPage - 1) * productsPerPage;
       const limit = productsPerPage;
       const response = await axios.get(
-        `https://api.escuelajs.co/api/v1/products/?price_min=${minPrice}&price_max=${maxPrice}&categoryId=${categoryId}&offset=${offset}&limit=${20}`
+        `https://api.escuelajs.co/api/v1/products/?price_min=${minPrice}&price_max=${maxPrice}&categoryId=${categoryId}&offset=${offset}&limit=${limit}`
       );
+      console.log("dataaaaaa",response.data)
       setProducts(response.data);
     } catch (error) {
       setError("Error fetching products. Please try again later.");
@@ -56,11 +198,16 @@ function CategoriesScreen() {
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
+  // Define categories
+  const categories = ["Clothing", "Accessories", "Electronics", "Books"]; // Add your own categories here
+
   return (
-    <div style={{marginleft:'10px',marginRight:'10px',backgroundColor:"#E9E6DF"}}>
+    <div style={{ marginLeft: "10px", marginRight: "10px", backgroundColor: "#E9E6DF" }}>
       <h1 style={{ textAlign: "center" }}>Categories</h1>
       <div style={{ display: "flex", justifyContent: "center", marginBottom: "20px" }}>
-        <button style={{ marginRight: "10px" }} onClick={() => handleSort("maxToMin")}>Max to Min Price</button>
+        <button style={{ marginRight: "10px" }} onClick={() => handleSort("maxToMin")}>
+          Max to Min Price
+        </button>
         <button onClick={() => handleSort("minToMax")}>Min to Max Price</button>
       </div>
 
@@ -75,54 +222,79 @@ function CategoriesScreen() {
         </label>
       </div>
 
+      {/* Category dropdown */}
+      <div style={{ textAlign: "center", marginBottom: "20px" }}>
+        <select value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)}>
+          <option value="">All Categories</option>
+          {categories.map((category, index) => (
+            <option key={index} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </div>
+
       {loading ? (
-        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "200px" }}>
-        </div>
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "200px" }}></div>
       ) : error ? (
         <p style={{ textAlign: "center", color: "red" }}>{error}</p>
       ) : (
-        <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))", gap: "20px" }}>
-          {currentProducts.map((product) => (
-            <div key={product.id} style={{ border: "1px solid #ccc", padding: "10px", textAlign: "center", borderRadius: '10px' }}>
-              {product.images && product.images.length > 0 && !product.images[0].startsWith('["') && !product.images[0].endsWith('"]') ? (
-                <img
-                  src={product.images[0]}
-                  alt={`Product 1`}
-                  height="150"
-                  width="auto"
-                  loading="lazy"
-                  style={{ borderRadius: '10px' }}
-                />
-              ) : (
-                <img
-                  src={defaultImage}
-                  alt="Image Not Found"
-                  height="150"
-                  width="auto"
-                  loading="lazy"
-                  style={{ borderRadius: '10px' }}
-                />
-              )}
-              <h3>{product.title}</h3>
-              <p>${product.price}</p>
-            </div>
-          ))}
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
+            gap: "20px",
+          }}
+        >
+          {currentProducts
+            .filter((product) => !selectedCategory || product.category === selectedCategory) // Filter products by selected category
+            .map((product) => (
+              <div
+                key={product.id}
+                style={{ border: "1px solid #ccc", padding: "10px", textAlign: "center", borderRadius: "10px" }}
+              >
+                {product.images && product.images.length > 0 && !product.images[0].startsWith('["') && !product.images[0].endsWith('"]') ? (
+                  <img
+                    src={product.images[0]}
+                    alt={`Product 1`}
+                    height="150"
+                    width="auto"
+                    loading="lazy"
+                    style={{ borderRadius: "10px" }}
+                  />
+                ) : (
+                  <img
+                    src={defaultImage}
+                    alt="Image Not Found"
+                    height="150"
+                    width="auto"
+                    loading="lazy"
+                    style={{ borderRadius: "10px" }}
+                  />
+                )}
+                <h3>{product.title}</h3>
+                <p>${product.price}</p>
+              </div>
+            ))}
         </div>
       )}
 
-      <ul style={{display: 'flex',
-            listStyle: 'none',
-        padding: '0px',
-            justifyContent: 'center',
-        marginTop: '20px',
-        margin:'20px',
-      }}>
-            {Array.from({ length: Math.ceil(products.length / productsPerPage) }).map((_, index) => (
-              <li key={index} className={currentPage === index + 1 ? 'active' : ''}>
-                <a onClick={() => paginate(index + 1)}>{index + 1}</a>
-              </li>
-            ))}
-          </ul>
+      <ul
+        style={{
+          display: "flex",
+          listStyle: "none",
+          padding: "0px",
+          justifyContent: "center",
+          marginTop: "20px",
+          margin: "20px",
+        }}
+      >
+        {Array.from({ length: Math.ceil(products.length / productsPerPage) }).map((_, index) => (
+          <li key={index} className={currentPage === index + 1 ? "active" : ""}>
+            <a onClick={() => paginate(index + 1)}>{index + 1}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
